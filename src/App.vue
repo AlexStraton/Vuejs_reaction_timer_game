94550eb8dc340133f80ee2e289e544d31e15a4ec
Alex Straton <alexandrastraton1@gmail.com>
2024-08-30 11:23:18 +0100

    created a project using vue and the two main components

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start">Play</button>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {
+      title: "my first Vue App",
+    };
+  },
+  methods: {
+    start() {
+      isCurrentlyPlaying = true;
+      setTimout(() => {
+        console.log("timer");
+      }, 4000);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
